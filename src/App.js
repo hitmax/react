896c971448a94ec3c9109e35464d0e184eb97c33
maksym.kyrylov <maksym.kyrylov@p2h.com>
2020-02-19 17:12:53 +0200

    3-6 done

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends React.Component {
 			textAlign: 'center'
 		}
 
-		const cars = this.state.cars;
+		// const cars = this.state.cars;
 
 		return (
 
@@ -45,19 +45,17 @@ class App extends React.Component {
 
 				<button onClick={this.changeTitleHandler.bind(this, 'Changed!')}>Change title</button>
 
-				<Car
-					name={cars[0].name}
-					year={cars[0].year}
-					onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)} />
-				<Car
-					name={cars[1].name}
-					year={cars[1].year}
-					onChangeTitle={() => this.changeTitleHandler(cars[1].name)} />
-				<Car
-					name={cars[2].name}
-					year={cars[2].year}
-					onChangeTitle={() => this.changeTitleHandler(cars[2].name)} />
-
+				{this.state.cars.map((car, index) => {
+					return (
+						<Car
+							key={index}
+							name={car.name}
+							year={car.year}
+							onChangeTitle={() => {
+								this.changeTitleHandler(car.name)
+							}} />
+					)
+				})}
 			</div>
 		);
 	}
